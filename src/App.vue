865e5c02362a72cd4d8aa5ab51216638263029db
Marty <marty@kingkit101.com>
2025-08-04 08:04:52 +0800

    fix package

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
   <div id="app">
-    <StagewiseToolbar v-if="isDevelopment" :config="stagewiseConfig" />
     <router-view />
   </div>
 </template>
 
 <script>
-import { StagewiseToolbar } from '@stagewise/toolbar-vue'
-
 export default {
-  name: 'App',
-  components: {
-    StagewiseToolbar
-  },
-  setup() {
-    // Stagewise配置 - 僅在開發模式使用
-    const isDevelopment = process.env.NODE_ENV === 'development'
-    const stagewiseConfig = {
-      plugins: []
-    }
-
-    return {
-      isDevelopment,
-      stagewiseConfig
-    }
-  }
+  name: 'App'
 }
 </script>
 
